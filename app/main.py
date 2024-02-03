c7dfc9b783ce84eb61331d1a79344000ea092aea
tommitt <tommytassi@hotmail.it>
2024-02-03 14:28:57 +0100

    Refactor app

--- app/main.py
+++ app/main.py
@@ -2,14 +2,16 @@ import cohere
 from langchain_community.chat_models import ChatCohere
 from langchain_core.prompts import ChatPromptTemplate
 
-from constants import COHERE_API_KEY, COHERE_MODEL
+from app.constants import COHERE_API_KEY, COHERE_MODEL
 
 MESSAGE = "Hello world!"
 SYSTEM_PROMPT = "You are a helpful assistant"
 
 # Langchain API
 llm = ChatCohere(model=COHERE_MODEL)
-prompt = ChatPromptTemplate.from_messages([("system", SYSTEM_PROMPT), ("human", "{message}")])
+prompt = ChatPromptTemplate.from_messages(
+    [("system", SYSTEM_PROMPT), ("human", "{message}")]
+)
 chain = prompt | llm
 response = chain.invoke({"message": MESSAGE})
 print(response.content)
