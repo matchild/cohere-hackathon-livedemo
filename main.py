f62145a00b5b86c29ebfd08666796ac3843f1961
tommitt <tommytassi@hotmail.it>
2024-02-03 12:16:07 +0100

    Use langchain prompt template and chain

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import cohere
 from langchain_community.chat_models import ChatCohere
+from langchain_core.prompts import ChatPromptTemplate
 
 from constants import COHERE_API_KEY, COHERE_MODEL
 
 MESSAGE = "Hello world!"
+SYSTEM_PROMPT = "You are a helpful assistant"
 
 # Langchain API
 llm = ChatCohere(model=COHERE_MODEL)
-response = llm.invoke(MESSAGE)
+prompt = ChatPromptTemplate.from_messages([("system", SYSTEM_PROMPT), ("human", "{message}")])
+chain = prompt | llm
+response = chain.invoke({"message": MESSAGE})
 print(response.content)
 
 
