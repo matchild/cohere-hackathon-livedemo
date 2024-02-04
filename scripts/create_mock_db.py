9257181aaf582d8bcc4149cc4237e203417ef97e
matchild <mt.child@gmx.com>
2024-02-04 12:28:29 +0100

    adds a second "CUSTOMERS" dataframe

--- scripts/create_mock_db.py
+++ scripts/create_mock_db.py
@@ -37,8 +37,74 @@ register_full_dataframe(
                     ),
                 ],
             ),
-        ],
-    ),
+
+            VariableFull(
+                name="contract_name",
+                description="Name given to the contract as shown in the finance database",
+                is_categorical=False,
+                values=[]
+            ),
+            VariableFull(
+                name="is_active",
+                description="Flag that keeps track of whether a contract is active or expired",
+                is_categorical=True,
+                values=[
+                    ValueLone(
+                        name="Active",
+                        description="The expected end of the contract, calculated as the the date it was signed on plus the expected duration, has not yet been reached"
+                    ),
+                    ValueLone(
+                        name="Expired",
+                        description="The expected end date of the contact has been reached"
+                    )
+                ]
+            )
+        ]
+    )
 )
 
+register_full_dataframe(
+    db,
+    DataframeFull(
+        name="CUSTOMERS",
+        description="This table contains all the information related to the customers",
+        variables=[
+            VariableFull(
+                name="customer_name",
+                description="Name of the customer",
+                is_categorical=False,
+                values=[]
+            ),
+            VariableFull(
+                name="customer_tax_code",
+                description="Tax code of the parent company",
+                is_categorical=False,
+                values=[]
+            ),
+            VariableFull(
+                name="location",
+                description="Geographical location of the client",
+                is_categorical=True,
+                values=[
+                    ValueLone(
+                        name="EU_West",
+                        description="Countries served in Western Europe: Italy, Spain, Portugal",
+
+                    ),
+                    ValueLone(
+                        name="EU_East",
+                        description="Countries served in Eastern Europe: Poland, Netherlands, Finland"
+                    ),
+                    ValueLone(
+                        name="US",
+                        description="Any of the states in the US"
+                    )
+                ]
+            )
+        ]
+    )
+)
+
+
 db.close()
+
