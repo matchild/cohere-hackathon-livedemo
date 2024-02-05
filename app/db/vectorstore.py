d8c117b7ceefda416469ab7fde64099b22170681
tommitt <tommytassi@hotmail.it>
2024-02-06 00:31:14 +0100

    Add doc to vectorstore function

--- app/db/vectorstore.py
+++ app/db/vectorstore.py
@@ -20,13 +20,31 @@ def get_vectorstore() -> Chroma:
     )
 
 
-def add_to_vectorstore(orm: DataframeORM | VariableORM | ValueORM) -> str:
+def orm_to_vectorstore(orm: DataframeORM | VariableORM | ValueORM) -> str:
+    """Add a SQL object to the vectorstore"""
     vectorstore = get_vectorstore()
 
     page_content = f"{orm.name}\n{orm.description}"
     doc = Document(
         page_content=page_content,
         metadata={"id": orm.id, "type": type(orm).__name__},
+    )
+
+    res = vectorstore.add_documents([doc])
+    return res[0]
+
+
+def doc_to_vectorstore(doc_in: dict[str, str]) -> str:
+    """
+    Add a document to the vectorstore.
+    The document is a dictionary with any number of key and value pairs - it must contain the "id" and "type" keys.
+    """
+    vectorstore = get_vectorstore()
+
+    page_content = "\n".join([f"{k}: {v}" for k, v in doc_in.items()])
+    doc = Document(
+        page_content=page_content,
+        metadata={"id": doc_in["id"], "type": doc_in["type"]},
     )
 
     res = vectorstore.add_documents([doc])
