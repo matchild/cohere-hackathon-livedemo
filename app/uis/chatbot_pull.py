60669aaa3686feec7d4733ed3203ebd951c71eb8
tommitt <tommytassi@hotmail.it>
2024-02-07 18:41:00 +0100

    Integrate connectors into pull chat UI

--- app/uis/chatbot_pull.py
+++ app/uis/chatbot_pull.py
@@ -1,30 +1,43 @@
 import streamlit as st
 
-# TODO: exchange with pull agent
-from app.agents.push import AiAgent
 from app.connectors.csv_connector import CSVConnector
 from app.connectors.pdf_connector import PDFConnector
-from app.db.services import get_dataframe_by_id
+
+# TODO: handle when all information are collected (save output to db, print success message and reset connector)
 
 
 def chat_ui_pull() -> None:
     """Chatbot UI for pull chat"""
 
     _CONTAINER_HEIGHT = 400
+    state = st.session_state
 
-    if "init_pull" not in st.session_state:
-        st.session_state["init_pull"] = True
-        st.session_state["messages_pull"] = []
+    if "init_pull" not in state:
+        state["init_pull"]: bool = True
+        state["messages_pull"]: list[dict[str, str]] = []
+        state["connector"]: CSVConnector | PDFConnector | None = None
+        state["inputs"]: dict[str, list[str]] = {
+            "inputs_required": [],
+            "inputs_required_prompts": [],
+        }
+        state["outputs"]: list[dict[str, str]] = []
 
     st.title("Pull Chatbot 🤖")
 
     uploaded_file = st.file_uploader("Upload your file", type=["csv", "pdf"])
-    if uploaded_file is not None:
+    if uploaded_file is not None and state["connector"] is None:
         if uploaded_file.type == "text/csv":
-            connector = CSVConnector(uploaded_file)
+            state["connector"] = CSVConnector(uploaded_file)
         elif uploaded_file.type == "application/pdf":
-            connector = PDFConnector(uploaded_file)
-        # TODO: do something with the connector
+            state["connector"] = PDFConnector(uploaded_file)
+
+        state["inputs"] = state["connector"].get_specifications()
+        state["messages_pull"].append(
+            {
+                "role": "ai",
+                "message": state["inputs"]["inputs_required_prompts"][0],
+            }
+        )
 
     col1, col2 = st.columns(2)
 
@@ -33,42 +46,44 @@ def chat_ui_pull() -> None:
 
         container1 = st.container(height=_CONTAINER_HEIGHT)
 
-        for message in st.session_state["messages_pull"]:
+        for message in state["messages_pull"]:
             container1.chat_message(message["role"]).markdown(message["message"])
 
         user_query = st.chat_input("Ask something...")
         if user_query:
             container1.chat_message("user").markdown(user_query)
 
-            with st.spinner("Thinking..."):
-                with st.session_state["conn"].session as db_session:
-                    agent = AiAgent(db=db_session)
-                    ai_answer = agent.run(
-                        user_query, chat_history=st.session_state["messages_pull"]
-                    )
+            state["outputs"].append({state["inputs"]["inputs_required"][0]: user_query})
+            state["inputs"]["inputs_required"] = state["inputs"]["inputs_required"][1:]
+            state["inputs"]["inputs_required_prompts"] = state["inputs"][
+                "inputs_required_prompts"
+            ][1:]
 
-            container1.chat_message("assistant").markdown(ai_answer)
-
-            st.session_state["messages_pull"] += [
+            state["messages_pull"] += [
                 {"role": "human", "message": user_query},
-                {"role": "ai", "message": ai_answer},
+                {
+                    "role": "ai",
+                    "message": state["inputs"]["inputs_required_prompts"][0],
+                },
             ]
+            st.rerun()
 
     with col2:
         st.header("Database")
 
         container2 = st.container(height=_CONTAINER_HEIGHT)
-        with st.session_state["conn"].session as db_session:
-            # TODO: use the right objects here
-            db_object = get_dataframe_by_id(db=db_session, id=1)
+        container2.json(state["outputs"])
 
-            obj_container = container2.container(border=True)
-            obj_container.write("Registered object:")
-            obj_container.json(
-                {
-                    "id": db_object.id,
-                    "name": db_object.name,
-                    "description": db_object.description,
-                    "registered_at": db_object.registered_at,
-                }
-            )
+        # with state["conn"].session as db_session:
+        # db_object = get_dataframe_by_id(db=db_session, id=1)
+
+        # obj_container = container2.container(border=True)
+        # obj_container.write("Registered object:")
+        # obj_container.json(
+        #     {
+        #         "id": db_object.id,
+        #         "name": db_object.name,
+        #         "description": db_object.description,
+        #         "registered_at": db_object.registered_at,
+        #     }
+        # )
