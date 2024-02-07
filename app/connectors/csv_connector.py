36264ab9623170a89f4111ad0b976c606cb82b7a
tommitt <tommytassi@hotmail.it>
2024-02-07 14:37:26 +0100

    Add csv uploader to SQL database

--- app/connectors/csv_connector.py
+++ app/connectors/csv_connector.py
@@ -1,8 +1,16 @@
+import logging
+
 import pandas as pd
+from sqlalchemy.orm import Session
+
+from app.db.schemas import DataframeFull, ValueLone, VariableFull
+from app.db.services import register_full_dataframe
 
 
 class CSVConnector:
-    def __init__(self, file):
+    _CATEGORICAL_THRESHOLD = 5
+
+    def __init__(self, file) -> None:
         self.columns_description = {}
         self.categorical_values_description = {}
         self.db_description = None
@@ -13,7 +21,7 @@ class CSVConnector:
         self.content = self.input_df.values.tolist()
         self.categorical_values = self.check_categoricals()
 
-    def save_data(self, data_dict: dict):
+    def save_data(self, data_dict: dict[str, str]) -> None:
         self.db_name = data_dict.pop("db_name")
         self.db_description = data_dict.pop("db_description")
         for column in self.column_names:
@@ -22,12 +30,12 @@ class CSVConnector:
                 self.categorical_values_description[cat_value] = data_dict[cat_value]
         self.check_columns()
 
-    def check_columns(self):
+    def check_columns(self) -> None:
         if not self.column_names:
             raise Exception("No header")
         elif not set(list(self.columns_description.keys())) == set(self.column_names):
-            print(set(list(self.columns_description.keys())))
-            print(set(self.column_names))
+            logging.info(set(list(self.columns_description.keys())))
+            logging.info(set(self.column_names))
             raise Exception("Column description not correct")
 
     def get_data_content(self) -> list:
@@ -49,17 +57,17 @@ class CSVConnector:
         cat_values_flat = []
         for column_name in self.column_names:
             columns_prompts.append(
-                "Describe the column "
+                "Describe the column '"
                 + column_name
-                + " of the dataset you just uploaded"
+                + "' of the dataset you just uploaded"
             )
             for cat_value in self.categorical_values.get(column_name, []):
                 columns_prompts.append(
-                    "Describe the meaning of categorical value "
+                    "Describe the meaning of categorical value '"
                     + cat_value
-                    + " for column "
+                    + "' for column '"
                     + column_name
-                    + " of the dataset you just uploaded"
+                    + "' of the dataset you just uploaded"
                 )
                 cat_values_flat.append(cat_value)
         specs = {
@@ -75,14 +83,40 @@ class CSVConnector:
         }
         return specs
 
-    def check_categoricals(self):
+    def check_categoricals(self) -> dict[str, str]:
         categorical_values = {}
         categorical_df = self.input_df.select_dtypes(include=["category", "object"])
         columns_categorical = categorical_df.columns.tolist()
         for column in columns_categorical:
-            if categorical_df[column].nunique() < 5:
+            if categorical_df[column].nunique() < self._CATEGORICAL_THRESHOLD:
                 categorical_values[column] = categorical_df[column].unique().tolist()
         return categorical_values
 
-    def upload_data(self):
-        pass
+    def upload_data(self, db: Session | None) -> None:
+        variables = []
+        for col in self.column_names:
+            values = []
+            is_categorical = col in self.categorical_values.keys()
+            if is_categorical:
+                for val in self.categorical_values[col]:
+                    values.append(
+                        ValueLone(
+                            name=val,
+                            description=self.categorical_values_description[val],
+                        )
+                    )
+            variables.append(
+                VariableFull(
+                    name=col,
+                    description=self.columns_description[col],
+                    is_categorical=is_categorical,
+                    values=values,
+                )
+            )
+        dataframe = DataframeFull(
+            name=self.db_name, description=self.db_description, variables=variables
+        )
+
+        logging.info(f"Uploading CSV to SQL database: {dataframe}")
+        if db is not None:
+            register_full_dataframe(db, dataframe)
--- tests/csv_connector_test/csv_connector_test.py
+++ tests/csv_connector_test/csv_connector_test.py
@@ -1,6 +1,9 @@
+import logging
 import os.path
 
 from app.connectors.csv_connector import CSVConnector
+
+logging.basicConfig(level=logging.INFO)
 
 path = os.path.abspath("biostats.csv")
 return_data = {
@@ -24,3 +27,5 @@ print(specifications)
 csvconn.save_data(data_dict=return_data)
 print(csvconn.get_data_info())
 # print(csvconn.get_data_content())
+
+csvconn.upload_data(db=None)
