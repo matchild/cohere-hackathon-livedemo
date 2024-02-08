5612fef25d5ab0e45d0a260c4c2e5db4aa06b4e5
tommitt <tommytassi@hotmail.it>
2024-02-08 20:25:56 +0100

    Revised pull agent conversationality

--- app/agents/pull.py
+++ app/agents/pull.py
@@ -1,32 +1,28 @@
 import logging
 
 import cohere
-from sqlalchemy.orm import Session
 
 from app.constants import COHERE_API_KEY
-from app.db.vectorstore import get_vectorstore
 
 
 class PullAgent:
-    _QUESTION_PROMPT = 'You will receive a request that starts with the tag "REQUEST:" followed by the request.' \
-                       'The request is about some data that the user gave as input. ' \
-                       'Your goal is to reformulate the request to improve it. ' \
-                       'The user is the expert this piece of data, so be specific. ' \
-                       'You can give hypotheses about what you think the request is about.' \
-                       'Remember that you have to bring the user to be as precise and complete as possible. ' \
-                       'Please keep the rephrased sentence concise and ' \
-                       'do not change the name of the variables contained in the request. ' \
-                       'Give in output only the rephrased request, without the tag "REQUEST:" and anything else'
+    _PROMPT = (
+        "Given the following request, generate a revised version that better fits into the conversation. "
+        "You are talking to a data expert who uploaded some data. "
+        "Your goal is to collect additional information throughout the conversation. "
+        "Use the following format: Revised request."
+        "Request: {request}"
+    )
 
-    def run(self, data_specification_request: str) -> str:
-        logging.info(f"Running push agent with user query: {data_specification_request}")
+    def run(self, request: str, chat_history: list[dict[str, str]] = []) -> str:
+        logging.info(f"Running pull agent with request: {request}")
+
         co = cohere.Client(COHERE_API_KEY)
-
-        complete_prompt = self._QUESTION_PROMPT + " REQUEST: " + data_specification_request
-
-        # generate search queries
-        response = co.generate(
-            prompt=complete_prompt
+        response = co.chat(
+            message=self._PROMPT.format(request=request), chat_history=chat_history
         )
-        print(response)
-        return response[0].text
+
+        if response.text[:17] == "Revised request: ":
+            return response.text[16:]
+
+        return response.text
--- app/uis/chatbot_pull.py
+++ app/uis/chatbot_pull.py
@@ -12,24 +12,23 @@ def chat_ui_pull() -> None:
 
     state = st.session_state
 
-    if "file_uploader_key" not in st.session_state:
-        st.session_state["file_uploader_key"] = 0
-
     if "init_pull" not in state:
         state["init_pull"]: bool = True
         state["messages_pull"]: list[dict[str, str]] = []
         state["connector"]: CSVConnector | PDFConnector | None = None
+        state["file_uploader_key"] = 0
         state["inputs"]: dict[str, list[str]] = {
             "inputs_required": [],
             "inputs_required_prompts": [],
         }
         state["outputs"]: list[dict[str, str]] = []
-        state["pull_agent"] = PullAgent()
 
     st.title("Pull Chatbot 🤖")
 
-    request_with_ai_on = st.toggle('Activate AI request')
-    uploaded_file = st.file_uploader("Upload your file", type=["csv", "pdf"], key=st.session_state["file_uploader_key"])
+    request_with_ai_on = st.toggle("Activate AI request")
+    uploaded_file = st.file_uploader(
+        "Upload your file", type=["csv", "pdf"], key=state["file_uploader_key"]
+    )
     if uploaded_file is not None and state["connector"] is None:
         if uploaded_file.type == "text/csv":
             state["connector"] = CSVConnector(uploaded_file)
@@ -38,12 +37,7 @@ def chat_ui_pull() -> None:
 
         state["inputs"] = state["connector"].get_specifications()
         next_message = state["inputs"]["inputs_required_prompts"][0]
-        state["messages_pull"].append(
-            {
-                "role": "ai",
-                "message": state["pull_agent"].run(next_message) if request_with_ai_on else next_message,
-            }
-        )
+        state["messages_pull"].append({"role": "ai", "message": next_message})
 
     col1, col2 = st.columns(2)
 
@@ -59,15 +53,27 @@ def chat_ui_pull() -> None:
         if user_query:
             container1.chat_message("user").markdown(user_query)
             if len(state["inputs"]["inputs_required_prompts"]) > 1:
-                state["outputs"].append({state["inputs"]["inputs_required"][0]: user_query})
-                state["inputs"]["inputs_required"] = state["inputs"]["inputs_required"][1:]
-                state["inputs"]["inputs_required_prompts"] = state["inputs"]["inputs_required_prompts"][1:]
+                state["outputs"].append(
+                    {state["inputs"]["inputs_required"][0]: user_query}
+                )
+                state["inputs"]["inputs_required"] = state["inputs"]["inputs_required"][
+                    1:
+                ]
+                state["inputs"]["inputs_required_prompts"] = state["inputs"][
+                    "inputs_required_prompts"
+                ][1:]
                 next_message = state["inputs"]["inputs_required_prompts"][0]
                 state["messages_pull"] += [
                     {"role": "human", "message": user_query},
                     {
                         "role": "ai",
-                        "message": state["pull_agent"].run(next_message) if request_with_ai_on else next_message,
+                        "message": (
+                            PullAgent().run(
+                                next_message, chat_history=state["messages_pull"]
+                            )
+                            if request_with_ai_on
+                            else next_message
+                        ),
                     },
                 ]
             elif state["connector"] is not None:
@@ -75,10 +81,12 @@ def chat_ui_pull() -> None:
                     {
                         "role": "ai",
                         "message": "All the useful information has been collected! Thanks for your help! "
-                                   "You can now close this window or upload another file",
+                        "You can now close this window or upload another file",
                     }
                 )
-                state["outputs"].append({state["inputs"]["inputs_required"][0]: user_query})
+                state["outputs"].append(
+                    {state["inputs"]["inputs_required"][0]: user_query}
+                )
                 state["connector"].save_data(state["outputs"])
                 with st.session_state["conn"].session as db_session:
                     state["connector"].upload_data(db=db_session)
@@ -93,7 +101,6 @@ def chat_ui_pull() -> None:
                     }
                 )
             st.rerun()
-
 
     with col2:
         st.header("Database")
