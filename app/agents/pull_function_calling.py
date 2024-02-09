0bd100eedd49457943d7b7f240af13e2748b07a4
Matteo Child <matteorobert.child@oliverwyman.com>
2024-02-09 17:53:28 +0100

    updates pull_function_calling

--- app/agents/pull_function_calling.py
+++ app/agents/pull_function_calling.py
@@ -5,7 +5,7 @@ import guardrails as gd
 from guardrails.validators import ValidRange, ValidChoices
 from pydantic import BaseModel, Field
 from rich import print
-from typing import List
+from typing import List, Optional
 import json 
 from rich import print
 
@@ -16,12 +16,25 @@ logging.basicConfig(level=logging.INFO)
 logging.info('Program starts')
 
 
+class VariableORM(BaseModel):
+    name: str = Field(..., description="Name of the column")
+    description: str = Field(..., description="Brief description of this column.")
+
+    def __repr__(self) -> str:
+        return f"VariableORM(id={self.id!r}, name={self.name!r})"
+
+
 class DataframeORM(BaseModel):
     name: str = Field(..., description="Name of the table", validators=[ValidChoices(["CONTRACTS", "CUSTOMERS"], on_fail="reask")])
     description: str = Field(..., description="Brief description of this table")
+    has_columns: bool
+    # variables: Optional[List[VariableORM]] = Field(None, description="Columns present in this table. Each column should be classified into a separate item in the list.")
 
     def __repr__(self) -> str:
         return f"DataframeORM(id={self.id!r}, name={self.name!r})"
+
+
+    
 
 
 expert_message = """The "CONTRACTS" table contains information about the contracts the company has signed in the last 5 years in Europe. It is the only source of truth we have for this kind of data."""
@@ -29,7 +42,7 @@ expert_message = """The "CONTRACTS" table contains information about the contrac
 
 
 PROMPT = """Given the following message from our company expert,
-please extract a dictionary that contains a summary of what the expert says. Try not to miss anything the expert says but above all, DO NOT MAKE UP ANYTHING!.
+please extract a dictionary that contains a summary of what the expert says. Try not to miss anything the expert says but above all, DO NOT MAKE UP ANYTHING AND DO NOT CREATE COLUMNS THAT DO NOT EXIST.
 Everything is to be written in English.
 
 ${expert_message}
@@ -54,4 +67,4 @@ res  = guard(
 # Print the validated output from the LLM
 print(json.dumps(res.validated_output, indent=2))
 
-# print(guard.history.last.iterations.first.rich_group)
+print(guard.history.last.iterations.first.rich_group)
